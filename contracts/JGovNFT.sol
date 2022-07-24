830ee7b58e7d74081c6d60926d514b99c2c57f2e
badoet <>
2022-07-24 15:46:58 +0800

    Update JGovNFT

--- contracts/JGovNFT.sol
+++ contracts/JGovNFT.sol
@@ -1,23 +1,61 @@
 // SPDX-License-Identifier: Nolicense
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/draft-ERC721VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract JGovNFT is ERC721, ERC721URIStorage, Ownable, EIP712, ERC721Votes {
-    using Counters for Counters.Counter;
+import "./IRoles.sol";
 
-    Counters.Counter private _tokenIdCounter;
+contract JGovNFT is 
+    Initializable,
+    ERC721Upgradeable,
+    ERC721EnumerableUpgradeable,
+    ERC721URIStorageUpgradeable,
+    PausableUpgradeable,
+    OwnableUpgradeable,
+    ERC721BurnableUpgradeable,
+    EIP712Upgradeable,
+    ERC721VotesUpgradeable
+{
+    using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    constructor(
-        string memory name,
-        string memory token,
-        string memory version
-    ) ERC721(name, token) EIP712(name, version) {}
+    CountersUpgradeable.Counter private _tokenIdCounter;
+    IRoles private rolesContract;
+
+    constructor() {
+        _disableInitializers();
+    }
+    
+    function initialize(string memory name, string memory token, string memory version) public initializer {
+        __ERC721_init(name, token);
+        __ERC721Enumerable_init();
+        __ERC721URIStorage_init();
+        __Pausable_init();
+        __Ownable_init();
+        __ERC721Burnable_init();
+        __EIP712_init(name, version);
+        __ERC721Votes_init();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
@@ -27,18 +65,29 @@ contract JGovNFT is ERC721, ERC721URIStorage, Ownable, EIP712, ERC721Votes {
     }
 
     // The following functions are overrides required by Solidity.
+     function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    )
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        whenNotPaused
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
 
     function _afterTokenTransfer(
         address from,
         address to,
         uint256 tokenId
-    ) internal override(ERC721, ERC721Votes) {
+    ) internal override(ERC721Upgradeable, ERC721VotesUpgradeable) {
         super._afterTokenTransfer(from, to, tokenId);
     }
 
     function _burn(uint256 tokenId)
         internal
-        override(ERC721, ERC721URIStorage)
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
     {
         super._burn(tokenId);
     }
@@ -46,9 +95,21 @@ contract JGovNFT is ERC721, ERC721URIStorage, Ownable, EIP712, ERC721Votes {
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(
+            ERC721Upgradeable,
+            ERC721EnumerableUpgradeable
+        )
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
